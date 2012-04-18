63ef145f4918bee19b53c33dbd62c2b25b42c655
romacron <info@romacron.de>
2012-04-18 19:22:21 +0200

    http://community.xajaxproject.org/topic/8603/bug-array-bug-detailed-bugreport/

--- tests/formSubmissionTest.php
+++ tests/formSubmissionTest.php
@@ -14,7 +14,8 @@ $xajax = new xajax();
 $xajax->register(XAJAX_FUNCTION, "testForm");
 $xajax->processRequest();
 $xajax->configure('javascript URI', '../');
-
+#$xajax->configure('useUncompressedScripts',true);
+$xajax->autoCompressJavaScript(null,true);
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
@@ -22,7 +23,6 @@ $xajax->configure('javascript URI', '../');
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title>Form Submission Test| xajax Tests</title>
-
 	<style type="text/css">
 		fieldset > div {
 			border           : 1px solid gray;
@@ -33,7 +33,6 @@ $xajax->configure('javascript URI', '../');
 
 	<?php $xajax->printJavascript("../") ?>
 </head>
-
 <body>
 <h2><a href="index.php">xajax Tests</a></h2>
 
@@ -45,119 +44,104 @@ $xajax->configure('javascript URI', '../');
 			<legend>
 				Test Form
 			</legend>
-
 			<div style="margin: 3px;">
 				<div>
 					Text Input
 				</div>
-
 				<input type="text" id="textInput" name="textInput" value="text" />
 			</div>
-
 			<div style="margin: 3px;">
 				<div>
 					Password Input
 				</div>
-
 				<input type="password" id="textInput" name="passwordInput" value="2br!2b" />
 			</div>
-
 			<div style="margin: 3px;">
 				<div>
 					Textarea
 				</div>
-
-				<textarea id="textarea" name="textarea">
-					text text
-				</textarea>
+				<textarea id="textarea" name="textarea">text text</textarea>
 			</div>
-
 			<div style="margin: 3px;">
 				<div>
 					<input type="checkbox"
 						   id="checkboxInput1" name="checkboxInput[]" value="1" checked="checked" />
-
 					<label for="checkboxInput1">Checkbox Input 1</label>
 				</div>
-
 				<div>
 					<input type="checkbox"
 						   id="checkboxInput2" name="checkboxInput[]" value="2" checked="checked" />
-
 					<label for="checkboxInput2">Checkbox Input 2</label>
 				</div>
 			</div>
-
 			<div style="margin: 3px;">
 				<div>
 					<input type="checkbox" id="checkboxMethodTwoInput1"
 						   name="checkboxMethodTwoInput[0]" value="1" checked="checked" />
-
 					<label for="checkboxMethodTwoInput1">Checkbox Method 2 Input 1</label>
 				</div>
-
 				<div>
 					<input type="checkbox" id="checkboxMethodTwoInput2"
 						   name="checkboxMethodTwoInput[1]" value="2" checked="checked" />
-
 					<label for="checkboxMethodTwoInput2">Checkbox Method 2 Input 2</label>
 				</div>
-
 				<div>
 					<input type="checkbox" id="checkboxMethodTwoInput3"
 						   name="checkboxMethodTwoInput[3]" value="4" checked="checked" />
-
 					<label for="checkboxMethodTwoInput3">Checkbox Method 2 Input 3</label>
 				</div>
 			</div>
+			<div style="margin: 3px;"><h3>Multi Dimensions Array</h3>
 
+				<div>
+					<input type="checkbox" id="multi1"
+						   name="multi[0]['test']" value="1" checked="checked" />
+					<label for="multi1">multi[0]['test']</label>
+				</div>
+				<div>
+					<input type="checkbox" id="multi2"
+						   name="multi[1]['test1']" value="2" checked="checked" />
+					<label for="multi2">multi[1]['test1']</label>
+				</div>
+				<div>
+					<input type="checkbox" id="multi3"
+						   name="multi[1]['test2']" value="4" checked="checked" />
+					<label for="multi3">multi[1]['test2']</label>
+				</div>
+			</div>
 			<div style="margin: 3px;">
 				<div>
 					Radio Input
 				</div>
-
 				<div>
 					<input type="radio"
 						   id="radioInput1" name="radioInput" value="1" checked="checked" />
-
 					<label for="radioInput1">One</label>
 				</div>
-
 				<div>
 					<input type="radio" id="radioInput2" name="radioInput" value="2" />
-
 					<label for="radioInput2">Two</label>
 				</div>
 			</div>
-
 			<div style="margin: 3px;">
 				<div>
 					Select
 				</div>
-
 				<select id="select" name="select">
 					<option value="1">One</option>
-
 					<option value="2">Two</option>
-
 					<option value="3">Three</option>
-
 					<option value="4">Four</option>
 				</select>
 			</div>
-
 			<div style="margin: 3px;">
 				<div>
 					Multiple Select
 				</div>
-
 				<select id="multipleSelect" name="multipleSelect[]" multiple="multiple" size=4>
 					<option value="1" selected="selected">One</option>
-
 					<option value="2">Two</option>
-
 					<option value="3">Three</option>
-
 					<option value="4">Four</option>
 				</select>
 			</div>
@@ -169,7 +153,6 @@ $xajax->configure('javascript URI', '../');
 		</fieldset>
 	</form>
 </div>
-
 <div id="submittedDiv" style=" margin: 3px;">
 </div>
 </body>
