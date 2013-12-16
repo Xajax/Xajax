5585f5d3f0e2cd2603634acef75015018f12945d
Ed robinson <edrobinsinjr@gmail.com>
2013-12-15 18:18:39 -0700

    Simple tool to compress the xajax_core.js after the uncompressed version has been modified. Simply open it with the browser. It won't say anything unless there is a problem. See the new modified date on the compressed version.

---
+++ acompress.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 2012-01-23
+ * Trying out html name array
+ */
+//Setup the xajax framework.
+include_once("xajax_core/xajax.inc.php");
+$xajax = new xajax();
+$xajax->autoCompressJavascript(NULL, true);
